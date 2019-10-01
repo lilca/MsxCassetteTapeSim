752d9e7183d75f71ab25b014b84930eb22c0d7ab
lilca <lucilectric999@gmail.com>
2019-10-01 13:33:47 +0900

    update comment state

--- msxcassettetape.py
+++ msxcassettetape.py
@@ -1,43 +1,43 @@
 class MsxCassetteTape:
-    baudrate = 1200     // 1200bps or 2400bps
+    baudrate = 1200     # 1200bps or 2400bps
     filename = ""
     tapedata = []
 
     def csave(self):
-        // File Header
+        # File Header
         self.playLongHeader()
         self.playByteRepeat(0xd3, 10)
         for idx in range(6):
             self.playByte(filename[idx])
-        // Interval
+        # Interval
         self.playInterval(1000)     // n[ms]
-        // File Body
+        # File Body
         self.playShortHeader()
         self.playByteArray(pointer, n)
         self.playByteRepeat(0x00, 7)
 
     def save(self):
-        // File Header
+        # File Header
         self.playLongHeader()
         self.playByteRepeat(0xea, 10)
         for idx in range(6):
             self.playByte(filename[idx])
-        // Interval
+        # Interval
         self.playInterval(1000)     // n[ms]
-        // File Body
+        # File Body
         while:
             self.playShortHeader()
             self.playByteArray(pointer, 256)
 
     def bsave(self):
-        // File Header
+        # File Header
         self.playLongHeader()
         self.playByteRepeat(0xd0, 10)
         for idx in range(6):
             self.playByte(filename[idx])
-        // Interval
+        # Interval
         self.playInterval(1000)     // n[ms]
-        // File Body
+        # File Body
         self.playShortHeader()
         self.playInt(off_address)
         self.playInt(last_address)
