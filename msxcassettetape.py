0f35d572591dc18ab263f0045a84c620386fb26d
lilca <lucilectric999@gmail.com>
2019-10-02 23:53:08 +0900

    Update msxcassettetape.py

--- msxcassettetape.py
+++ msxcassettetape.py
@@ -2,19 +2,20 @@ import WaveFile
 
 class MsxCassetteTape:
     def __init__(self):
-        baudrate = 1200     # 1200bps or 2400bps
-        filename = ""
-        tapedata = []
+        self.baudrate = 1200     # 1200bps or 2400bps
+        self.samplerate = 44100
+        self.filename = ""
+        self.tapedata = []
 
     def setArray(self, array):
         for idx in range(len(array)):
             self.tapedata[idx] = array[idx]*4+3
     def playShortHeader(self, wavefile):
-        wavefile.extendSquareWave()
-    def playLongHeader(self):
-
-    def csave(self, filename6):
-        wave = WaveFile(44100, 1)
+        wavefile.extendSquareWave(self.baudrate * 2, 50, 1700)
+    def playLongHeader(self, wavefile):
+        wavefile.extendSquareWave(self.baudrate * 2, 50, 6700)
+    def csave(self):
+        wave = WaveFile(self.samplerate)
         # File Header
         self.playLongHeader(wave)
         self.playByteRepeat(0xd3, 10)
@@ -27,7 +28,7 @@ class MsxCassetteTape:
         self.playByteArray(pointer, n)
         self.playByteRepeat(0x00, 7)
 
-    def save(self, filename6):
+    def save(self):
         # File Header
         self.playLongHeader()
         self.playByteRepeat(0xea, 10)
@@ -40,7 +41,7 @@ class MsxCassetteTape:
             self.playShortHeader()
             self.playByteArray(pointer, 256)
 
-    def bsave(self, filename6, startaddr, endaddr,runaddr):
+    def bsave(self, startaddr, endaddr,runaddr):
         # File Header
         self.playLongHeader()
         self.playByteRepeat(0xd0, 10)
